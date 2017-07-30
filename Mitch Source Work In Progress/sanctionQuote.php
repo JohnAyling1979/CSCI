c89ec339fd55796054100b5a90d3ecb15c50ba60
Mitch Myers <mlmyers09@gmail.com>
2017-07-30 14:07:54 -0500

    Update sanctionQuote.php

--- Mitch Source Work In Progress/sanctionQuote.php
+++ Mitch Source Work In Progress/sanctionQuote.php
@@ -44,7 +44,7 @@ require "dbconnect.php";
 
     	if (isset($_POST["quoteId"]))
         {
-			while(($row = $result->fetch()) != NULL)
+	    while(($row = $result->fetch()) != NULL)
             {
                 echo "<b>Sales Associate: </b>";
                 echo $row["salesAssociate"];
@@ -52,10 +52,10 @@ require "dbconnect.php";
                 echo "<b>Quote ID: </b>";
                 echo $row["quoteId"];
                 echo "<br><br>";
-       		    echo "<b>Customer Name: </b>";
+       		echo "<b>Customer Name: </b>";
                 echo $row["customerName"];
                 echo "<br>";
-       		    echo "<b>Customer Address: </b>";
+       		echo "<b>Customer Address: </b>";
                 echo $row["customerAddress"];
                 echo "<br>";
                 echo "<b>Customer City: </b>";
@@ -64,8 +64,8 @@ require "dbconnect.php";
                 echo "<b>Customer Email: </b>";
                 echo $row["customerEmail"];
                 echo "<br>";
-       		}
-		}
+       	    }
+	}
 
     ?>
 
