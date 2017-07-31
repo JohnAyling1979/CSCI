366bc52a760c8fbea83f2d50ece4e8e4a2bb452d
JohnAyling1979 <JohnAyling@yahoo.com>
2017-07-31 17:10:21 -0500

    no change

--- Mitch Source Work In Progress/PurchaseOrderGateway.php
+++ Mitch Source Work In Progress/PurchaseOrderGateway.php
@@ -1,12 +1,20 @@
 <?php
     class PurchaseOrderGateway
     {
-
-
+        /*******************************************************************
+            FUNCTION:   PurchaseOrderGateway::getDateAndRate
+            ARGUMENTS:  $data: and array contianing order, Associate, custId,
+                        and total amount
+            RETURNS:    a php object which contains the date and rate
+            USAGE:      To send the PO info to the processor and get the date
+                        and commistion rate
+        *******************************************************************/
         public function getDateAndRate($data)
         {
+            //PO system address
             $url = 'http://blitz.cs.niu.edu/PurchaseOrder/';
 
+            //array to hold the data
             $options = array
             (
                 'http' => array
@@ -21,9 +29,11 @@
                 )
             );
 
+            //create the connection and get the result
             $context  = stream_context_create($options);
             $result = file_get_contents($url, false, $context);
 
+            //returns the decoded data
             return json_decode($result);
         }
     }
