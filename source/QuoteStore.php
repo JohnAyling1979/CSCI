0a40d080832e90e3c941eae80cb1a11713ce0075
John Ayling <johnayling@yahoo.com>
2017-08-03 20:26:51 -0500

    Update QuoteStore.php

--- source/QuoteStore.php
+++ source/QuoteStore.php
@@ -100,8 +100,89 @@
             return $isCreated;
         }
 
-        public function getQuote()
+        /*******************************************************************
+            FUNCTION:   QuoteStore::getSanctionedQuotes
+            ARGUMENTS:  none
+            RETURNS:    a PDO statment containing the sactioned quotes
+            USAGE:      to find all the sactioned quotes
+        *******************************************************************/
+        public function getSanctionedQuotes()
         {
+            $DB=$this->connect();
+            $query="select quoteId,customerName from Quote where isSanctioned";
+
+            return $DB->query($query);
+        }
+
+        /*******************************************************************
+            FUNCTION:   QuoteStore::getQuote
+            ARGUMENTS:  $quoteId: unique id of the quote
+            RETURNS:    an array containing the quote information
+            USAGE:      to get the information of a quote
+        *******************************************************************/
+        public function getQuote($quoteId)
+        {
+            $DB=$this->connect();
+            $query="select * from Quote where quoteId=$quoteId";
+
+            $stmt=$DB->query($query);
+
+            return $stmt->fetch();
+
+        }
+        
+        /*******************************************************************
+            FUNCTION:   QuoteStore::getLineItems()
+            ARGUMENTS:  $quoteId: unique id of the quote
+            RETURNS:    a pdo statement contain all the line items
+            USAGE:      to get the line items attached to the quote
+        *******************************************************************/
+        public function getLineItems($quoteId)
+        {
+            $DB=$this->connect();
+            $query="select * from LineItem where quoteId=$quoteId";
+            
+            return $DB->query($query);
+        }
+
+        /*******************************************************************
+            FUNCTION:   QuoteStore::createPurchaseOrder
+            ARGUMENTS:  $quoteId: unique id of the quote
+                        $price: new discounted price
+            RETURNS:    none
+            USAGE:      to update the cost of the quote an change it to a PO
+        *******************************************************************/
+        public function createPurchaseOrder($quoteId,$price)
+        {
+            $DB=$this->connect();
+
+            $query="update Quote set currentPrice=$price where quoteId=$quoteId";
+            $DB->query($query);
+
+            $query="update Quote set isSanctioned=0 where quoteId=$quoteId";
+            $DB->query($query);
+
+            $query="update Quote set isPO=1 where quoteId=$quoteId";
+            $DB->query($query);
+        }
+
+        /*******************************************************************
+            FUNCTION:   QuoteStore::setDateAndCommission
+            ARGUMENTS:  $quoteId: unique id of the quote
+                        $processDay: process day for the quote
+                        $commission: commission value
+            RETURNS:    none
+            USAGE:      to set the proccess date and commission
+        *******************************************************************/
+        public function setDateAndCommission($quoteId,$processDay,$commission)
+        {
+            $DB=$this->connect();
+
+            $query="update Quote set processingDate='$processDay' where quoteId=$quoteId";
+            $DB->query($query);
+
+            $query="update Quote set commission=$commission where quoteId=$quoteId";
+            $DB->query($query);
         }
 
         public function updateQuote()
@@ -132,15 +213,7 @@
         {
         }
 
-        public function createPurchaseOrder()
-        {
-        }
-
         public function getFinizedQuote()
-        {
-        }
-
-        public function getSanctionedQuote()
         {
         }
     }
