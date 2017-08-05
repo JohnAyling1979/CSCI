42769aa3a982c200714c483970d555ac390ad78b
CJyoder <cjyoder16@gmail.com>
2017-08-04 22:02:43 -0500

    Delete editSearch.php

--- CJ Source Work/AdminGUI/editSearch.php
+++
@@ -1,8 +0,0 @@
-<h2>Edit sales associate</h2>
-
-<h3>Enter Search</h3>
-            
-<form method=POST action="admin.php?page=editList">
-<input type=text name=search>
-<button>Find</button>
-</form>
