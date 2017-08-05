b6bf113006192ee215226f2cd5866e85ad69f85b
CJyoder <cjyoder16@gmail.com>
2017-08-05 15:31:03 -0500

    Delete deleteSearch.php

--- CJ Source Work/AdminGUI/deleteSearch.php
+++
@@ -1,8 +0,0 @@
-<h2>Delete sales associate</h2>
-
-<h3>Search for sales associate</h3>
-            
-<form method=POST action="admin.php?page=deleteList">
-<input type=text name=search>
-<button>Find</button>
-</form>
