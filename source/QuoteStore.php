25e96d47b9591f87d79b876f54c8905d7c24edd5
CJyoder <cjyoder16@gmail.com>
2017-08-06 14:45:05 -0500

    added functions needed for admin

--- source/QuoteStore.php
+++ source/QuoteStore.php
@@ -215,5 +215,100 @@
         public function getFinizedQuote()
         {
         }
+        
+        /*******************************************************************
+            FUNCTION:   getCustomerNames
+            ARGUMENTS:  none
+            RETURNS:    name of customers in a list
+            USAGE:      To be able to retreve a list of Customers
+        *******************************************************************/
+		public function getCustomerNames()
+        {
+			//connects to database
+            $db=$this->connect();;
+            //creates query
+            $query="select * from Quote group by custId;";
+			return $db->query($query);
+        }
+		
+		/*******************************************************************
+            FUNCTION:   getQuotsStatus
+            ARGUMENTS:  none	
+            RETURNS:    all the quots by a given status
+            USAGE:      To be able to search for quots by a given status
+        *******************************************************************/
+		public function getQuotsStatus($Stype)
+        {
+			$one="1";
+			//connects to database
+            $db=$this->connect();;
+            //creates query
+            $query="select * from Quote where " . $Stype . " = '$one';";
+			return $db->query($query);
+        }
+		
+		/*******************************************************************
+            FUNCTION:   getQuotsByCust
+            ARGUMENTS:  none	
+            RETURNS:    all the quots by a given status
+            USAGE:      To be able to search for quots by a given status
+        *******************************************************************/
+		public function getQuotsByCust($custId)
+        {
+			//connects to database
+            $db=$this->connect();;
+            //creates query
+            $query="select * from Quote where custId = '$custId';";
+			return $db->query($query);
+        }
+		
+		/*******************************************************************
+            FUNCTION:   getQuotsBySA
+            ARGUMENTS:  none	
+            RETURNS:    all the quots by a given status
+            USAGE:      To be able to search for quots by a given status
+        *******************************************************************/
+		public function getQuotsBySA($assoc)
+        {
+			//connects to database
+            $db=$this->connect();;
+            //creates query
+            $query="select * from Quote where salesAssociate LIKE  '$assoc';";
+			return $db->query($query);
+        }
+		
+		/*******************************************************************
+            FUNCTION:   SAstore::getFinalQuote
+            ARGUMENTS:  $name: Name of the associate
+            RETURNS:    1 quote
+            USAGE:      to view a quote
+        *******************************************************************/
+		
+		public function getFinalQuote($quoteId)
+        {
+            //connects to database
+            $db=$this->connect();
+            //creates query
+            $query="select * from Quote where quoteId = '$quoteId';";
+            return $db->query($query);
+        }
+        
+        /*******************************************************************
+            FUNCTION:   getDates
+            ARGUMENTS:  none	
+            RETURNS:    all quots in date range
+            USAGE:      Get quots in a date range
+        *******************************************************************/
+		public function getDates($start,$end)
+        {
+			//connects to database
+            $db=$this->connect();;
+			$new_start = date('Y/n/j', strtotime($start));
+			$new_end = date('Y/n/j', strtotime($end));
+			
+            //creates query
+			$query="select * from Quote where isPO and STR_TO_DATE(processingDate,'%Y/%m/%d') > STR_TO_DATE('$new_start','%Y/%m/%d') and STR_TO_DATE(processingDate,'%Y/%m/%d') < STR_TO_DATE('$new_end','%Y/%m/%d');";
+			return $db->query($query);
+        }
     }
 ?>
