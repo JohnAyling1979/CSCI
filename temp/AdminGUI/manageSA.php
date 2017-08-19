c4bde452f72e939c4ee01fa70ef2402f45ef0a18
Mitch Myers <mlmyers09@gmail.com>
2017-08-18 20:18:43 -0500

    Delete manageSA.php

--- temp/AdminGUI/manageSA.php
+++
@@ -1,7 +0,0 @@
-<center><h3>Please select how you want to manage a sales associate</h3></center>
-
-<p  align="center">
- [ <a href="?page=editSearch">Edit Sales Associate</a> ]
- [ <a href="?page=creatSA">Creat Sales Associate</a> ]
- [ <a href="?page=deleteSearch">Delete Sales Associate</a> ]
-<p>
